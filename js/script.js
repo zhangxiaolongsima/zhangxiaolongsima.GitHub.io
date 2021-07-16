8107bb7d2948c32726c92168252812d4ad5eba5a
zhangxiaolongsima <zhangxiaolongsima@protonmail.com>
2021-07-16 22:49:43 +0800

    Site updated: 2021-07-16 22:49:43

--- js/script.js
+++ js/script.js
@@ -89,7 +89,20 @@
         var bannerNode = $('.top-image');
         if(bannerNode.data('enable')){
             var index = parseInt((Math.random() * 4) + 1);
-            bannerNode.attr('style','background-image:url(https://cdn.jsdelivr.net/gh/zhangxiaolongsima/zhangxiaolongsima.github.io/banner/1.jpg)');
+			var sUserAgent = navigator.userAgent.toLowerCase();
+            var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+            var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+            var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+            var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+            var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+            var bIsAndroid = sUserAgent.match(/android/i) == "android";
+            var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+            var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+			if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) ){
+          bannerNode.attr('style','background-image:url(https://cdn.jsdelivr.net/gh/zhangxiaolongsima/zhangxiaolongsima.github.io/banner/1.jpg)');
+        }else{
+		  bannerNode.attr('style','background-image:url(https://cdn.jsdelivr.net/gh/zhangxiaolongsima/zhangxiaolongsima.github.io/banner/2.jpg)');
+		}
         }
     })
 
